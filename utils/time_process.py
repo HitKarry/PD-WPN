99a46f67efc71128ae2cd01a20eb84d9a1e2efaf
HitKarry <karry@stu.hit.edu.cn>
2025-05-29 22:05:36 +0800

    Update time_process.py

--- utils/time_process.py
+++ utils/time_process.py
@@ -31,19 +31,9 @@ def dataset_generator(dataset,history_size,target_size, rolling_step=1, sampling
     return np.array(data), np.array(labels)
 
 
-for year in range(2018,2024):
-    wrf = xr.open_dataset('./1_wind_10m_100m_'+str(year)+'.nc')
-    temp = np.array(wrf.valid_time)
-    if year==2018:
-        data = temp
-    else:
-        data = np.concatenate([data,temp],0)
-
-np.savez('1_wind_data_45n_35n_90e_100e_time.npz',data=np.array(data))
-
 history_size,target_size,rolling_step,sampling_step,single_step = 24,24,5,1,False
 
-data_npz = np.load('1_wind_data_45n_35n_90e_100e_time.npz')
+data_npz = np.load('wind_data_time.npz')
 data = data_npz['data']
 data,labels = dataset_generator(data,history_size,target_size, rolling_step, sampling_step, single_step)
 np.savez('wind_data_slide_time.npz',data_x=np.array(data),data_y=np.array(labels))
